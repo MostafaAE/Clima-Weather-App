76f39bc2f30e654b6c2d44cb64dad203db4a51d0
Mostafa Ayman <mostafa.aymanae@gmail.com>
2021-04-18 16:40:11 +0200

    networking services

---
+++ lib/services/networking.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class NetworkHelper {
+  NetworkHelper(this.url);
+  final String url;
+
+  Future<dynamic> getWeatherData() async {
+    Uri parsedUrl = Uri.parse(url);
+
+    http.Response response = await http.get(parsedUrl);
+    if (response.statusCode == 200)
+      return jsonDecode(response.body);
+    else
+      print(response.statusCode);
+  }
+}
