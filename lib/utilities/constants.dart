2c7a93bbdf2fa957f4d914bca27cc3ea5c109b0b
Mostafa Ayman <mostafa.aymanae@gmail.com>
2021-04-23 21:40:38 +0200

    CityScreen implementation

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -21,3 +21,23 @@ const kCityTextStyle = TextStyle(
   fontSize: 45.0,
   fontWeight: FontWeight.bold,
 );
+
+const kTextFieldInputDecoration = InputDecoration(
+  filled: true,
+  fillColor: Colors.white,
+  icon: Icon(
+    Icons.location_city,
+    color: Colors.white,
+    size: 35,
+  ),
+  hintText: 'Enter City Name',
+  hintStyle: TextStyle(
+    color: Colors.grey,
+  ),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(10.0),
+    ),
+    borderSide: BorderSide.none,
+  ),
+);
